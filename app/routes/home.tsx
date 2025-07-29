2fd44e2510a72df3bae8711d80815e2f818c83c7
devjeiku <jakebasa0428@gmail.com>
2025-07-29 15:31:02 +0800

    feat: add puter auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import type { Route } from './+types/home';
 import Navbar from '~/components/Navbar';
-import { resumes } from '../../constants';
 import ResumeCard from '~/components/ResumeCard';
+import { usePuterStore } from '~/lib/puter';
+import { Link, useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import { resumes } from '../../constants';
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -11,9 +14,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated]);
+
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
             <Navbar />
+
             <section className='main-section'>
                 <div className='page-heading py-16'>
                     <h1>Track Your Applications & Resume Ratings</h1>
